dfe3b38346b05f0958d35eccce747ecf18c1a061
RajanDagar <46718537+RajanDagar@users.noreply.github.com>
2021-11-21 17:12:13 +0530

    port change

--- index.js
+++ index.js
@@ -2,12 +2,11 @@ const express = require("express");
 const mediasoup = require("mediasoup");
 const cors = require("cors");
 const config = require("./config");
-const port = 3000;
 
 const app = express();
 app.use(cors({origin: true})); // Allowing Cross-Origin Requests
 
-app.get("/Router", async (req, res) => {
+app.get("/router", async (req, res) => {
   try {
     const worker = await mediasoup.createWorker({
       logLevel: config.mediasoup.worker.logLevel,
@@ -30,6 +29,40 @@ app.get("/Router", async (req, res) => {
   }
 });
 
-app.listen(port, () => {
+app.get("/createTransport", async (req, res) => {
+  try {
+    let { participantId, direction } = req.body;
+
+    let transport = await createWebRtcTransport({ participantId, direction });
+    roomState.transports[transport.id] = transport;
+
+    let { id, iceParameters, iceCandidates, dtlsParameters } = transport;
+    res.send({
+      transportOptions: { id, iceParameters, iceCandidates, dtlsParameters }
+    });
+  } catch (e) {
+    res.send({error: e});
+  }
+});
+
+async function createWebRtcTransport({ participantId, direction }) {
+  const {
+    listenIps,
+    initialAvailableOutgoingBitrate
+  } = config.mediasoup.webRtcTransport;
+
+  const transport = await router.createWebRtcTransport({
+    listenIps: listenIps,
+    enableUdp: true,
+    enableTcp: true,
+    preferUdp: true,
+    initialAvailableOutgoingBitrate: initialAvailableOutgoingBitrate,
+    appData: { participantId, clientDirection: direction }
+  });
+
+  return transport;
+}
+
+app.listen(5000,'127.0.0.1', () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
